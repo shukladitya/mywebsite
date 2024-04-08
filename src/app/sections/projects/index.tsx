21b82999ce449a6ecf8effbdc994d5f78e408f23
Aditya Shukla <62618138+shukladitya@users.noreply.github.com>
2024-04-08 10:59:22 +0000

    tulsidas banner updated

--- src/app/sections/projects/index.tsx
+++ src/app/sections/projects/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import Mountain from "../../../assets/Mountain.jpg";
+import Tulsidas from '../../../assets/Tulsidas.jpg'
 import Link from "@/components/Link";
 const Projects = () => {
   return (
@@ -13,11 +14,11 @@ const Projects = () => {
           <Link text="Code" url="http://instagram.com" />
         </div>
       </div>
-      <div className={styles.parallax}>
-        <Image src={Mountain} alt="Mountain" className={styles.parallaxBG} />
+      <div className={`${styles.parallax} ${styles.margin}`}>
+        <Image src={Tulsidas} alt="Tuslidas" className={styles.parallaxBG} />
         <div className={styles.linkPanel}>
           <Link text="View" url="http://google.com" />
-          <Link text="Download" url="http://fb.com" />
+          <Link text="How To?" url="http://fb.com" />
           <Link text="Code" url="http://instagram.com" />
         </div>
       </div>
